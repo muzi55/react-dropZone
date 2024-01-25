6f904de186e81346cb1889ec598307901ec98568
muzi55 <muzi5@kakao.com>
2024-01-25 23:22:19 +0900

    드랍존 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div></div>
+      <main>
+        <h1>react dropzone</h1>
+
+        <div className="card-box">
+          <div className="card">
+            <p>
+              <span>Click to upload</span> or drag and drop
+            </p>
+            <p>PNG, JPG (max, 800 X 500px)</p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
