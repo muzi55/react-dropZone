294cc852897527d21a496645384b802382cc0c1d
muzi55 <muzi5@kakao.com>
2024-01-25 23:42:04 +0900

    dropzone imgdrop 시 스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useDropzone } from "react-dropzone";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log(acceptedFiles[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
     <>
       <main>
         <h1>react dropzone</h1>
+        <input type="file" name="" id="" {...getInputProps()} />
 
-        <div className="card-box">
+        <div className={`card-box ${isDragActive && "active"}`} {...getRootProps()}>
           <div className="card">
             <p>
               <span>Click to upload</span> or drag and drop
